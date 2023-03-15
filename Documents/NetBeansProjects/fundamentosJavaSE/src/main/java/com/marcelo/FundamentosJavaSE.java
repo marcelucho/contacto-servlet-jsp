be7984289a58779c7290b1e32cddfa77e4095734
marcelucho <marcelucho007@gmail.com>
2023-03-15 20:25:15 -0300

    commit inicial

---
+++ Documents/NetBeansProjects/fundamentosJavaSE/src/main/java/com/marcelo/FundamentosJavaSE.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.marcelo;
+
+/**
+ *
+ * @author Luis
+ */
+public class FundamentosJavaSE {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java APP!");
+    }
+}
