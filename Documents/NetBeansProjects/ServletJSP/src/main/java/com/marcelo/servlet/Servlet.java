cb3fc1c877392d685f630b2d0e371ea5f817347e
marcelucho <marcelucho007@gmail.com>
2023-04-12 20:55:17 -0400

    Servlet doGet

---
+++ Documents/NetBeansProjects/ServletJSP/src/main/java/com/marcelo/servlet/Servlet.java
@@ -0,0 +1,19 @@
+package com.marcelo.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// extends forma parte de la herencia
+@WebServlet(urlPatterns="/inicio")  // para acceder al servlet el usuario localhost:8080/inicio
+public class Servlet extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest resq, HttpServletResponse resp) throws ServletException, IOException{
+        resp.getWriter().append("Hola Mundo desde DOget otra vez");
+    }
+    
+}
